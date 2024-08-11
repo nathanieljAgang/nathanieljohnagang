efee88758f5fe9c5f5ce548a795b0043beed54dc
nathanieljAgang <165679034+nathanieljAgang@users.noreply.github.com>
2024-08-11 12:26:34 +0100

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,59 +1,71 @@
-function App() {
-  const [expression, setExpression] = React.useState("");
-  const [answer, setAnswer] = React.useState("0")
-  const display = (Symbol)=>{
-    setExpression(prev => prev + Symbol)
-    if(expression(expression.length-1) === "="){
-      if(/[1-9]/.test(Symbol)){
-        setExpression(Symbol)
-      }else{
-        setExpression(answer + Symbol);
-      }
+$(document).ready(function () {
+    var result = 0;
+    var prevEntry = 0;
+    var operation = null;
+    var currentEntry = "0";
+    updateScreen(operation);
+
+    $('.button').on('click', function(evt) {
+        var buttonPressed = $(this).html();
+        console.log(buttonPressed);
+
+        if (buttonPressed === "C") {
+            result = 0;
+            currentEntry = "0";
+        } else if (buttonPressed === "CE") {
+            currentEntry = "0";
+        } else if (buttonPressed === "back") {
+            currentEntry = currentEntry.substring(0, currentEntry.length - 1);
+        } else if (buttonPressed === "+/-") {
+            currentEntry *= -1;
+        } else if (buttonPressed === '.') {
+            currentEntry += '.';
+        } else if (isNumber(buttonPressed)) {
+            if (currentEntry === '0') currentEntry = buttonPressed;
+            else currentEntry = currentEntry + buttonPressed;
+        } else if (isOperator(buttonPressed)) {
+            prevEntry = parseFloat(currentEntry);
+            operation = buttonPressed;
+            currentEntry = '';
+        } else if (buttonPressed === '%') {
+            currentEntry = currentEntry / 100;
+        } else if (buttonPressed === "sqrt") {
+            currentEntry = Math.sqrt(currentEntry);
+        } else if (buttonPressed === "1/x") {
+            currentEntry = 1 / currentEntry;
+        } else if (buttonPressed === "pi") {
+            currentEntry = Math.PI;
+        } else if (buttonPressed === "=") {
+            currentEntry = calculate(prevEntry, parseFloat(currentEntry), operation);
+            operation = null;
+        }
+        updateScreen(currentEntry);
+    });
+
+    function isNumber(value) {
+        return !isNaN(value);
     }
-  };
-  const calculate = () => {
-    setAnswer(eval(expression));
-    setExpression((prev)=>prev + "=")
-  };
-  
-  const allClear = ()=>{
-setExpression("");
-setAnswer("");
-  }
-  const clear = () => {
-    setExpression(prev => prev.split("").slice(0, prev.length - 1).join(""));
-    setAnswer("0");
-  };
-  
-    return (
-      <div className="container">
-        <div className="grid">
-          <div className="display">
-<input type="text" value={expression} placeholder="0" disabled></input>
-<div className="Total">{answer}</div>
-          </div>
-          <button onClick={allClear} className="padbutton AC natty">AC</button>
-          <button onClick={clear} className="padbutton C natty">C</button>
-          <button onClick={()=>display("/")} className="padbutton divide">/</button>
-          <button onClick={()=>display("*")} className="padbutton times">*</button>
-          <button onClick={()=>display("7")} className="padbutton seven dark-gray">7</button>
-          <button onClick={()=>display("8")} className="padbutton eight dark-gray">8</button>
-          <button onClick={()=>display("9")} className="padbutton nine dark-gray">9</button>
-          <button onClick={()=>display("-")} className="padbutton minus dark-gray">-</button>
-          <button onClick={()=>display("4")} className="padbutton four dark-gray">4</button>
-          <button onClick={()=>display("5")} className="padbutton five dark-gray">5</button>
-          <button onClick={()=>display("6")} className="padbutton six dark-gray">6</button>
-          <button onClick={()=>display("+")} className="padbutton plus dark-gray">+</button>
-          <button onClick={()=>display("1")} className="padbutton one dark-gray">1</button>
-          <button onClick={()=>display("2")} className="padbutton two dark-gray">2</button>
-          <button onClick={()=>display("3")} className="padbutton three dark-gray">3</button>
-          <button onClick={calculate} className="padbutton equals Agang">=</button>
-          <button onClick={()=>display("0")} className="padbutton zero">0</button>
-          <button onClick={()=>display(".")} className="padbutton dot">.</button>
-        </div>
-      </div>
-    );
-  }
-  
-  ReactDOM.render(<App />, document.getElementById("root"));
-  
+
+    function isOperator(value) {
+        return value === '+' || value === '-' || value === '*' || value === '/';
+    }
+
+    function calculate(prev, curr, op) {
+        switch (op) {
+            case '+':
+                return prev + curr;
+            case '-':
+                return prev - curr;
+            case '*':
+                return prev * curr;
+            case '/':
+                return prev / curr;
+            default:
+                return curr;
+        }
+    }
+
+    function updateScreen(value) {
+        $('.screen').html(value);
+    }
+});
